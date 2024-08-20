87ccd090e557fe28653169654c248bd742d3a8ee
hale-Dennis <haledennis0@gmail.com>
2024-08-20 14:24:45 -0700

    add all code for simple web security api

---
+++ web-security/src/main/java/com/dennis/web/security/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.dennis.web.security.controller;
+
+
+
+import com.dennis.web.security.model.User;
+import org.owasp.encoder.Encode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@Validated @RequestBody User user) {
+        // Input validation is automatically handled by @Validated
+        users.add(user);
+        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<String> getUserByEmail(@PathVariable("email") String email) {
+        // Output encoding
+        String safeEmail = Encode.forHtml(email);
+
+        // Simulate finding a user
+        for (User user : users) {
+            if (user.getEmail().equalsIgnoreCase(safeEmail)) {
+                return new ResponseEntity<>("User found: " + Encode.forHtml(user.getName()), HttpStatus.OK);
+            }
+        }
+
+        return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+    }
+
+}
+
