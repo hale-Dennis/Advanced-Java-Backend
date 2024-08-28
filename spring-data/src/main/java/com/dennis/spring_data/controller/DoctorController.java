37f6b733dd44d4efda0f6ef54377a6b19f8ebe15
hale-Dennis <haledennis0@gmail.com>
2024-08-28 11:26:55 -0700

    spring data lab2 done

---
+++ spring-data/src/main/java/com/dennis/spring_data/controller/DoctorController.java
@@ -0,0 +1,63 @@
+package com.dennis.spring_data.controller;
+
+import com.dennis.spring_data.model.Doctor;
+import com.dennis.spring_data.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/v1/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+
+    @PostMapping
+    public Doctor createDoctor(@RequestBody Doctor doctor) {
+        return doctorService.createDoctor(doctor);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id) {
+        Optional<Doctor> doctor = doctorService.getDoctorById(id);
+        return doctor.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/specialty")
+    public List<Doctor> getDoctorsBySpecialty(@RequestParam String specialty) {
+        return doctorService.getDoctorsBySpecialty(specialty);
+    }
+
+    public List<Doctor> getDoctorsByLastName(@RequestParam String lastName) {
+        return doctorService.getDoctorByLastName(lastName);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Doctor> updateDoctor(@PathVariable Long id, @RequestBody Doctor doctorDetails) {
+        try {
+            Doctor updatedDoctor = doctorService.updateDoctor(id, doctorDetails);
+            return ResponseEntity.ok(updatedDoctor);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
