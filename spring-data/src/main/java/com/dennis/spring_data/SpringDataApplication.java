3f8b655690a19cabd73cc6f15c694bdfc2a300ad
hale-Dennis <haledennis0@gmail.com>
2024-08-29 17:00:37 -0700

    spring data lab3 done

--- spring-data/src/main/java/com/dennis/spring_data/SpringDataApplication.java
+++ spring-data/src/main/java/com/dennis/spring_data/SpringDataApplication.java
@@ -2,8 +2,12 @@ package com.dennis.spring_data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching
 public class SpringDataApplication {
 
 	public static void main(String[] args) {
