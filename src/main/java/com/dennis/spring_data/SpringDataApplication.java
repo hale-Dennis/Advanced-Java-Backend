c878cbb8696aa97dc2ec900a103395d7b44998e8
hale-Dennis <haledennis0@gmail.com>
2024-09-24 09:39:45 -0700

    final mod for jenkins pipeline

--- src/main/java/com/dennis/spring_data/SpringDataApplication.java
+++ src/main/java/com/dennis/spring_data/SpringDataApplication.java
@@ -2,11 +2,10 @@ package com.dennis.spring_data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
 
 @SpringBootApplication
-@EnableCaching
 public class SpringDataApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dennis/spring_data/controller/DoctorController.java
+++ src/main/java/com/dennis/spring_data/controller/DoctorController.java
@@ -1,64 +1,18 @@
 package com.dennis.spring_data.controller;
 
-import com.dennis.spring_data.model.Doctor;
-import com.dennis.spring_data.service.DoctorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/doctors")
 public class DoctorController {
 
-    @Autowired
-    private DoctorService doctorService;
-
-
-    @PostMapping
-    public HttpStatus createDoctor(@RequestBody Doctor doctor) {
-        return doctorService.createDoctor(doctor);
+    @GetMapping()
+    public String getDoctors() {
+        return "Doctor List";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id) {
-        Optional<Doctor> doctor = doctorService.getDoctorById(id);
-        return doctor.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @GetMapping
-    public List<Doctor> getAllDoctors() {
-        return doctorService.getAllDoctors();
-    }
-
-    @GetMapping("/specialty")
-    public List<Doctor> getDoctorsBySpecialty(@RequestParam String specialty) {
-        return doctorService.getDoctorsBySpecialty(specialty);
-    }
-
-    public List<Doctor> getDoctorsByLastName(@RequestParam String lastName) {
-        return doctorService.getDoctorByLastName(lastName);
-    }
-
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Doctor> updateDoctor(@PathVariable Long id, @RequestBody Doctor doctorDetails) {
-        try {
-            Doctor updatedDoctor = doctorService.updateDoctor(id, doctorDetails);
-            return ResponseEntity.ok(updatedDoctor);
-        } catch (RuntimeException e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
-        doctorService.deleteDoctor(id);
-        return ResponseEntity.noContent().build();
-    }
 }
 
