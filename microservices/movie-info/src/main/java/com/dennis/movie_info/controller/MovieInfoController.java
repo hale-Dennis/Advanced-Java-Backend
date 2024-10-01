5291491376e7b484f8ce792be6ed95ce801c43fb
hale-Dennis <haledennis0@gmail.com>
2024-10-01 10:05:15 -0700

    basic 3 apps for microservice

---
+++ microservices/movie-info/src/main/java/com/dennis/movie_info/controller/MovieInfoController.java
@@ -0,0 +1,18 @@
+package com.dennis.movie_info.controller;
+
+
+import com.dennis.movie_info.model.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoController {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId, "Severance");
+    }
+}
---
+++ microservices/movie-rating/src/test/java/com/dennis/movie_rating/MovieRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennis.movie_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
